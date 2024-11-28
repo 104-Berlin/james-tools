8e57edab0661c79e15ff3b4a72e1e2472d5469d5
Luca <james.lol2000@gmail.com>
2024-11-28 16:35:50 +0100

    change home

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
         <div>
             <h1 className="font-extrabold text-3xl p-8">
-                Home
+                Home hallo dennis
             </h1>
         </div>
     )
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,25 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+//import basicSsl from '@vitejs/plugin-basic-ssl'
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl({
+  plugins: [react(), /*basicSsl({
     name: 'localhost',
     domains: ['*.localhost'],
     certDir: '../backend/',
-  })],
+  })*/],
   server: {
-    https: true,
     proxy: {
       '/api': {
-        target: 'https://localhost:42069',
+        target: 'http://localhost:42069',
         changeOrigin: true,
         secure: false,
       },
       '/uploads': {
-        target: 'https://localhost:42069',
+        target: 'http://localhost:42069',
         changeOrigin: true,
         secure: false,
       }
