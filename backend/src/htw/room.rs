fd3d4f0d0dc35de05088743c5f643204ed02685d
Luca <james.lol2000@gmail.com>
2024-11-28 10:09:11 +0100

    Try running requests with self signed cert

--- backend/src/htw/room.rs
+++ backend/src/htw/room.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+use reqwest::Client;
 use scraper::{Html, Selector};
 
 use crate::models::htw_rooms::{Room, TimeEntry};
@@ -14,8 +15,16 @@ pub async fn get_time_entries(room: &Room) -> Vec<TimeEntry> {
         "https://lsf.htw-berlin.de/qisserver/rds?state=wplan&act=Raum&pool=Raum&raum.rgid={}",
         room.request_id
     );
+    let cert = std::fs::read("wildcard-self-signed.pem").expect("Reading self signed wildcard");
+    let cert = reqwest::Certificate::from_pem(&cert).expect("Parsing certificate");
 
-    let html = reqwest::get(url)
+    let html = Client::builder()
+        .use_rustls_tls()
+        .add_root_certificate(cert)
+        .build()
+        .unwrap()
+        .get(url)
+        .send()
         .await
         .expect("Failed to send request")
         .text()
@@ -138,8 +147,16 @@ pub async fn get_time_entries(room: &Room) -> Vec<TimeEntry> {
 
 pub async fn fetch_rids() -> Vec<FetchedRoom> {
     let url = "https://lsf.htw-berlin.de/qisserver/rds?state=wsearchv&search=3&raum.gebid=1112&choice.k_campus.id=y&k_campus.id=4&P_start=0&P_anzahl=1000&_form=display";
+    let cert = std::fs::read("wildcard-self-signed.pem").expect("Reading self signed wildcard");
+    let cert = reqwest::Certificate::from_pem(&cert).expect("Parsing certificate");
 
-    let html = reqwest::get(url)
+    let html = Client::builder()
+        .use_rustls_tls()
+        .add_root_certificate(cert)
+        .build()
+        .unwrap()
+        .get(url)
+        .send()
         .await
         .expect("Failed to send request")
         .text()
